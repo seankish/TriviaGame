f23f50a530b1bfab50bfdb78e76b2d4ece5aa5ad
skish1987 <seankish1@gmail.com>
2019-06-06 13:23:11 -0700

    timer and buttons working

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,1 +1,39 @@
-javascript
+$(document).ready(function() {
+    $(".submit").hide();
+    $(".questions").hide();
+    $("#results").hide();
+    $(".timer").hide();
+    var wrongAnswer = 0;
+    var correctAnswer = 0;
+    var timer = 60;
+    console.log("ready");
+    $(".start-button").on("click",function() {
+        alert("clicked");
+        $(".submit").show();
+        $(".questions").show()
+        $(".timer").show();
+        $(".timer").text("Time Remaining:ll " + timer)
+        setTimeout(timeUp, 1000 * 5);
+        function timeUp () {
+            alert("time is up")
+            
+            
+        }
+    })
+
+//     // set timer for trivia test
+//     // setTimeout(timeUp, 1000*60);
+// $(".start-button").on(click, function() {
+//     setTimeout(timeUp, 1000*60);
+//     alert("started")
+//     function timeUp() {
+//         console.log("times up")
+//         alert("times up")
+//     }
+// });
+
+
+
+
+
+});
